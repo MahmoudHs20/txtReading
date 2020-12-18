f6ddd30b82f99593de2d0136dc0fcdaa0787fd85
Mahmoud Hussien <56605228+MahmoudHs20@users.noreply.github.com>
2020-12-18 10:13:57 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 
 window.onload = () =>{
  document.getElementById("btn").onclick = () =>{
+  console.log("sad");
   $.getJSON("test.json", function(json) {
     console.log(json); // this will show the info it in firebug console
 });
