89301634ebbcf5afea610cd8c1ceae2cbeb6bc44
Mahmoud Hussien <56605228+MahmoudHs20@users.noreply.github.com>
2020-12-18 10:54:32 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,7 @@
- async function getData(){
-  const response = await fetch("test.json").then( res => res.json() );
- console.log(response);
-  return response;
-}
 window.onload = ()=>{
- document.getElementById("btn").onclick = function(){
-   const data =  getData();
-  console.log(data);
+ document.getElementById("btn").onclick = () => {
+    $.getJSON("test.json", function(json) {
+    console.log(json);
+});
 }
 }
