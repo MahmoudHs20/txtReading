08c6aa692c8e4fce5047cd4ce51c8f241916aec5
Mahmoud Hussien <56605228+MahmoudHs20@users.noreply.github.com>
2020-12-18 10:19:31 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,7 @@
 window.onload = () =>{
  document.getElementById("btn").onclick = () =>{
   console.log("sad");
-  fetch("test.json").then( res => console.log(res); );
+  fetch("test.json").then(function(res){
+    console.log(res);
+  });
 }
